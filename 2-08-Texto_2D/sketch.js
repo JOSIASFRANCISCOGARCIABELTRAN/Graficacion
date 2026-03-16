0c63d46704f5e3869d6a59f05d6d32a59a9ac919
Garcia Beltran Josias Francisco <l24170557@culiacan.tecnm.mx>
2026-03-16 20:54:38 +0000

    Puntos 6 a 8

---
+++ 2-08-Texto_2D/sketch.js
@@ -0,0 +1,17 @@
+function draw() {
+  createCanvas(600, 400);
+  background(240);
+
+  let s = 1 + 0.5 * sin(frameCount * 0.05);
+
+  push();
+  translate(width / 2, height / 2);
+
+  scale(s);
+
+  textAlign(CENTER, CENTER);
+  textSize(40);
+  text("Escalando", 0, 0);
+
+  pop();
+}
